5c8eef9813467d13f61cbfc6bd95cf14df5e319e
Doron Malka <drnmalka@gmail.com>
2023-11-13 22:40:40 +0200

    almost finished anim_tree + have movement working

---
+++ Player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+
+const ACCELERATION = 80
+const MAX_SPEED = 200
+const FRICTION = 500
+
+@onready var anim_tree = $AnimationTree
+@onready var anim_player = $AnimationPlayer
+@onready var anim_state = $AnimationTree.get("parameters/playback")
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		anim_tree.set("parameters/Idle/blend_position", input_vector)
+		anim_tree.set("parameters/Run/blend_position", input_vector)
+		anim_state.travel("Run")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		anim_state.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	print(velocity)
+	move_and_slide()
