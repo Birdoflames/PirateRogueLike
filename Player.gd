2d52e109d87c19f0af8f93a976323ff16fe3f9f1
Birdoflames <drnmalka@gmail.com>
2023-11-16 13:54:56 +0200

    animations tree working

--- Player.gd
+++ Player.gd
@@ -18,10 +18,12 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		anim_tree.set("parameters/Idle/blend_position", input_vector)
 		anim_tree.set("parameters/Run/blend_position", input_vector)
-		anim_state.travel("Run")
+		anim_tree.set("parameters/conditions/is_moving", true)
+		anim_tree.set("parameters/conditions/idle", false)
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		anim_state.travel("Idle")
+		anim_tree.set("parameters/conditions/is_moving", false)
+		anim_tree.set("parameters/conditions/idle", true)
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	print(velocity)
 	move_and_slide()
